e659ce97088ac54b5b9042423b1a6ed3313f080e
“LeomaraACgit config --global user.name “LeomaraAC <leomara.andrade@gmail.com>
2018-12-08 21:16:59 -0200

    Adicionando campo para data.

--- app/src/main/java/com/example/ifsp/easymoney/AddDividaActivity.java
+++ app/src/main/java/com/example/ifsp/easymoney/AddDividaActivity.java
@@ -40,31 +40,84 @@ public class AddDividaActivity extends AppCompatActivity {
         ed_data = (EditText) findViewById(R.id.ed_data);
         ed_valor = (EditText) findViewById(R.id.ed_valor);
         ed_desc = (EditText) findViewById(R.id.ed_desc);
+        ed_data.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar calendar = Calendar.getInstance();
+                int ano = calendar.get(Calendar.YEAR);
+                int mes = calendar.get(Calendar.MONTH);
+                int dia = calendar.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog dialog = new DatePickerDialog(
+                        AddDividaActivity.this,
+                        onDateSetListener,
+                        ano, mes, dia
+                );
+                dialog.show();
+            }
+        });
 
+        onDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int ano, int mes, int dia) {
+                String data = dia +"/"+ (mes+1) +"/"+ano;
+                ed_data.setText(data);
+            }
+        };
         btn_addDivida.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                addDivida();
+                String data = ed_data.getText().toString().trim();
+                String desc = ed_desc.getText().toString().trim();
+                String valorString = ed_valor.getText().toString().trim();
+                if (!data.equals("") && !desc.equals("") && !valorString.equals("")) {
+                    if (desc.length() <= 255) {
+                        Float valor = Float.parseFloat(valorString);
+                        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+                        try {
+                            Date date = format.parse(data);
+                            if (valor > 0) {
+                                if (new Date().after(date)) {
+                                    addDivida(valor, data, desc);
+                                }else {
+                                    Toast.makeText(AddDividaActivity.this, "A data da dívida deve ser anterior a data atual.", Toast.LENGTH_SHORT).show();
+                                }
+
+                            } else {
+                                Toast.makeText(AddDividaActivity.this, "O valor da dívida deve ser maior do que 0.", Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (ParseException e) {
+                            e.printStackTrace();
+                        }
+                    } else
+                        Toast.makeText(AddDividaActivity.this, "A descrição deve ter no máximo 255 caracteres.", Toast.LENGTH_SHORT).show();
+                }
+                else
+                    Toast.makeText(AddDividaActivity.this,
+                            "Todos os campos devem ser preenchidos.",
+                            Toast.LENGTH_SHORT).show();
             }
         });
 
     }
 
 
-    private void addDivida(){
+    private void addDivida(Float valor, String data, String desc){
         database = openOrCreateDatabase("easy_money", MODE_PRIVATE, null);
         Cursor cursor = database.rawQuery("SELECT nome " +
                 "FROM devedor WHERE id="+devedorId, null);
-       if(cursor != null){
-           database.execSQL("INSERT INTO divida (id_devedor,data,descricao,valor) VALUES (idDevedor,data,descricao,valor)");
-           database.close();
-           finish();
-       }else{
-           Toast.makeText(this, "Erro ao inserir divida.", Toast.LENGTH_SHORT).show();
-       }
+        if(cursor != null){
+            database.execSQL("INSERT INTO divida (id_devedor,data,descricao,valor) VALUES ("
+                    + devedorId+",'" +
+                    data +"','" +
+                    desc +"'," +
+                    valor+")");
+            database.close();
+            finish();
+            Toast.makeText(this, "Dívida inserida com sucesso.", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "Erro ao inserir divida.", Toast.LENGTH_SHORT).show();
+        }
         database.close();
-
-
     }
 
 
